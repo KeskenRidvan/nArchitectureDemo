634a235e84cb8dd34e374e3431da26a0726a9c67
Ridvan Kesken <kesken.dev@gmail.com>
2024-06-29 02:20:32 +0300

    Entity Framework implementation was made.

--- src/corePackages/Core.Persistence/Repositories/Entity.cs
+++ src/corePackages/Core.Persistence/Repositories/Entity.cs
@@ -1,5 +1,6 @@
-﻿namespace Core.Persistence.Repositories;
-public class Entity<TId>
+﻿
+namespace Core.Persistence.Repositories;
+public class Entity<TId> : IEntityTimestamps
 {
     public TId Id { get; set; }
     public DateTime CreatedDate { get; set; }
