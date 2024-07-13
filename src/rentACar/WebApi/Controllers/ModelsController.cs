163b10296cff441d525e1076ca24279eb182fa26
Ridvan Kesken <kesken.dev@gmail.com>
2024-07-14 01:05:29 +0300

    The implementation of our include infrastructure was done.

---
+++ src/rentACar/WebApi/Controllers/ModelsController.cs
@@ -0,0 +1,23 @@
+﻿using Application.Features.Models.Queries.GetList;
+using Core.Application.Requests;
+using Core.Application.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[Route("api/models")]
+[ApiController]
+public class ModelsController : BaseController
+{
+    [HttpGet]
+    public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+    {
+        GetListModelQuery getListModelQuery = new()
+        {
+            PageRequest = pageRequest
+        };
+
+        GetListResponse<GetListModelResponseDto> response = await Mediator.Send(getListModelQuery);
+        return Ok(response);
+    }
+}
