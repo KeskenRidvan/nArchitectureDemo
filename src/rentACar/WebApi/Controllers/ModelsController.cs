62f6c013c0dd0571d286314cde28bb3673058a90
Ridvan Kesken <kesken.dev@gmail.com>
2024-07-14 01:22:36 +0300

    Dynamic infrastructure was implemented.

--- src/rentACar/WebApi/Controllers/ModelsController.cs
+++ src/rentACar/WebApi/Controllers/ModelsController.cs
@@ -1,6 +1,8 @@
 ﻿using Application.Features.Models.Queries.GetList;
+using Application.Features.Models.Queries.GetListByDynamic;
 using Core.Application.Requests;
 using Core.Application.Responses;
+using Core.Persistence.Dynamic;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Controllers;
@@ -18,6 +20,19 @@ public class ModelsController : BaseController
         };
 
         GetListResponse<GetListModelResponseDto> response = await Mediator.Send(getListModelQuery);
+        return Ok(response);
+    }
+
+    [HttpPost("getList/byDynamic")]
+    public async Task<IActionResult> GetListByDynamic([FromQuery] PageRequest pageRequest, [FromBody] DynamicQuery? dynamicQuery = null)
+    {
+        GetListByDynamicModelQuery getListByDynamicModelQuery = new()
+        {
+            PageRequest = pageRequest,
+            DynamicQuery = dynamicQuery
+        };
+
+        GetListResponse<GetListByDynamicModelResponseDto> response = await Mediator.Send(getListByDynamicModelQuery);
         return Ok(response);
     }
 }
