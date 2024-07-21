b2e286460fdd170db9867c96fff1166140f35728
Ridvan Kesken <kesken.dev@gmail.com>
2024-07-21 15:52:02 +0300

    - Entity configurations were made. - Repository implementations were made.

--- src/rentACar/Persistence/PersistenceServiceRegistiration.cs
+++ src/rentACar/Persistence/PersistenceServiceRegistiration.cs
@@ -19,6 +19,13 @@ public static class PersistenceServiceRegistiration
         services.AddScoped<IModelRepository, ModelRepository>();
         services.AddScoped<ITransmissionRepository, TransmissionRepository>();
 
+        services.AddScoped<IEmailAuthenticatorRepository, EmailAuthenticatorRepository>();
+        services.AddScoped<IOperationClaimRepository, OperationClaimRepository>();
+        services.AddScoped<IOtpAuthenticatorRepository, OtpAuthenticatorRepository>();
+        services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IUserOperationClaimRepository, UserOperationClaimRepository>();
+
         return services;
     }
 }
